15e77b5d87703aba5ec1632a1c00b337d813e711
Spomky <florent@morselli.fr>
2020-12-23 17:03:34 +0100

    First release

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     WebPush\Bundle\WebPushBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
